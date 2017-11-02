38c4d4d73d5aee4871bab1d2703e9b844f10e81e
Travis Geis <travis@qualia.com>
2017-11-02 10:00:25 -0700

    Depend on Meteor version instead of specific package versions

--- package.js
+++ package.js
@@ -1,26 +1,28 @@
 Package.describe({
   name: 'qualia:try-modify',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Construct multi-document Mongo modifiers transactionally',
   git: 'https://github.com/qualialabs/try-modify',
   documentation: 'README.md'
 });
 
 var dependencies = [
-  'ecmascript@0.9.0',
+  'ecmascript',
 ];
 
 Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.4');
   api.use(dependencies);
   api.mainModule('lib.js');
 });
 
 Package.onTest(function(api) {
+  api.versionsFrom('METEOR@1.4');
   api.use(dependencies);
   api.use([
-    'tinytest@1.0.12',
-    'practicalmeteor:sinon@1.10.3_2',
-    'qualia:try-modify@0.0.2',
+    'tinytest',
+    'practicalmeteor:sinon',
+    'qualia:try-modify',
   ]);
   api.mainModule('test.js');
 });
